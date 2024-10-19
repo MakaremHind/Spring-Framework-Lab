d5171186a85fd41892c06fdcea69c65894a46698
Hind MAKAREM <hind.a.makarem@gmail.com>
2024-10-19 16:35:08 +0200

    Custom DAO

--- src/main/java/com/emse/spring/automacorp/dao/WindowDao.java
+++ src/main/java/com/emse/spring/automacorp/dao/WindowDao.java
@@ -4,18 +4,13 @@ import com.emse.spring.automacorp.model.WindowEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface WindowDao extends JpaRepository<WindowEntity, Long> {
-
-    List<WindowEntity> findByRoomId(Long roomId); // Custom method for finding windows by room ID.
-
-    @Query("select w from WindowEntity w where w.name=:name")
-    WindowEntity findByName(@Param("name") String name);
+public interface WindowDao extends JpaRepository<WindowEntity, Long>, WindowDaoCustom {
+    List<WindowEntity> findByRoomId(Long roomId);
 
     @Modifying
-    @Query("delete from WindowEntity w where w.name = ?1")
-    void deleteByName(String name);
+    @Query("delete from WindowEntity w where w.room.id = ?1")
+    void deleteByRoom(Long roomId);
 }
--- src/test/java/com/emse/spring/automacorp/dao/WindowDaoTest.java
+++ src/test/java/com/emse/spring/automacorp/dao/WindowDaoTest.java
@@ -1,24 +1,39 @@
 package com.emse.spring.automacorp.dao;
 
-import com.emse.spring.automacorp.dao.WindowDao;
-import com.emse.spring.automacorp.model.WindowEntity;
 import com.emse.spring.automacorp.model.WindowEntity;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@DataJpaTest // 
+import java.util.List;
+
+@DataJpaTest
 class WindowDaoTest {
-    @Autowired // 
+
+    @Autowired
     private WindowDao windowDao;
 
     @Test
-    public void shouldFindAWindowById() {
-        WindowEntity window = windowDao.getReferenceById(-10L); //
-        Assertions.assertThat(window.getName()).isEqualTo("Window 1");
-        Assertions.assertThat(window.getWindowStatus().getValue()).isEqualTo(1.0);
+    public void shouldFindRoomsWithOpenWindows() {
+        List<WindowEntity> result = windowDao.findRoomsWithOpenWindows(-10L);
+        Assertions.assertThat(result)
+                .hasSize(1)
+                .extracting("id", "name")
+                .containsExactlyInAnyOrder(Tuple.tuple(-10L, "Window 1"));
+    }
+
+    @Test
+    public void shouldNotFindRoomsWithOpenWindows() {
+        List<WindowEntity> result = windowDao.findRoomsWithOpenWindows(-9L);
+        Assertions.assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void shouldDeleteWindowsRoom() {
+        windowDao.deleteByRoom(-10L);
+        List<WindowEntity> result = windowDao.findByRoomId(-10L);
+        Assertions.assertThat(result).isEmpty();
     }
 }
